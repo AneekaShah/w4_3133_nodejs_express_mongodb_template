0c2fc4499e18eebe25202fdca1346158cdd7d23b
Aneeka <aneeka_shahnewaz@hotmail.com>
2022-02-03 17:35:18 -0500

    updated index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json()); // Make sure it comes back as json
 
 //TODO - Replace you Connection String here
-mongoose.connect('mongodb+srv://aneeka:GBC123@comp3123.wxxv0.mongodb.net/employee?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://aneeka:GBC123@comp3123.wxxv0.mongodb.net/employees?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(success => {
